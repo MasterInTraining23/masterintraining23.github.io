76e109801277e089980fa750887c656df4e28188
MasterInTraining23 <amadu23@gmail.com>
2019-12-24 18:11:45 -0800

    Adding initial pages

---
+++ battlestar_demo_main.js
@@ -0,0 +1,13 @@
+﻿console.log("battlestar main");
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('file:///C:/Users/glori/Documents/battlestar_demo/battlestar_demo_service_worker.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
