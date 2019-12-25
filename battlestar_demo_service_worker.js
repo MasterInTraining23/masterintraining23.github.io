56c7a42a42bd6bbc1d8e56c21e13e5ef6676f211
MasterInTraining23 <amadu23@gmail.com>
2019-12-25 01:44:25 -0800

    Update battlestar_demo_service_worker.js

--- battlestar_demo_service_worker.js
+++ battlestar_demo_service_worker.js
@@ -1,6 +1,6 @@
 ﻿self.addEventListener('install', event => {
   console.log("msg from Client", event);
-  self.clients.matchAll().then(clients => {
+  this.clients.matchAll().then(clients => {
     console.log("a client", client);
     clients.forEach(client => client.postMessage('hello from the other side'));
   });
