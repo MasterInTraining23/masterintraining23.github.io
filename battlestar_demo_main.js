ca72b738a22d1793e21e26b33c301bce4764a39b
MasterInTraining23 <amadu23@gmail.com>
2019-12-25 13:01:10 -0800

    Implement cache and removing debugger

--- battlestar_demo_main.js
+++ battlestar_demo_main.js
@@ -11,7 +11,7 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/battlestar_demo_service_worker.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      debugger;
+      console.log('registration.active:', registration.active);
       registration.active.postMessage({msgType: 'SAVE_PORT'}, [messageChannel.port2]);
     }, function(err) {
       // registration failed :(
