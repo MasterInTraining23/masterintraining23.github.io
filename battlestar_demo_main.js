09bd129ccd42c6c56465277ef471630e0f5a842e
MasterInTraining23 <amadu23@gmail.com>
2019-12-25 09:52:34 -0800

    Pass communication channel at initialization

--- battlestar_demo_main.js
+++ battlestar_demo_main.js
@@ -1,15 +1,15 @@
-﻿if ('serviceWorker' in navigator) {
+﻿const messageChannel = new MessageChannel();
+messageChannel.port1.onmessage = event => { console.log('msg from SW', event); }
+
+if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('/battlestar_demo_service_worker.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      registration.active.postMessage("save channel for communication", [messageChannel.port2]);
     }, function(err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
   });
-
-  window.addEventListener("message", event => {
-    console.log("msg from SW", event);
-  }, false);
 }
--- battlestar_demo_service_worker.js
+++ battlestar_demo_service_worker.js
@@ -5,12 +5,14 @@
     // Exit early if we don't have access to the client.
     // Eg, if it's cross-origin.
     if (!event.clientId) return;
+    console.log("we have access to the client");
 
     // Get the client.
     const client = await clients.get(event.clientId);
     // Exit early if we don't get the client.
     // Eg, if it closed.
     if (!client) return;
+    console.log("there's a client to retrieve");
 
     // Send a message to the client.
     client.postMessage({
