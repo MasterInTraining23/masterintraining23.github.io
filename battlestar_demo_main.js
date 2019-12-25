fbc64434cc5c776f65117ea356fdf77bb92c6fc5
MasterInTraining23 <amadu23@gmail.com>
2019-12-25 10:58:37 -0800

    Support race condition communication

--- battlestar_demo_main.js
+++ battlestar_demo_main.js
@@ -6,10 +6,12 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/battlestar_demo_service_worker.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      registration.active.postMessage("save channel for communication", [messageChannel.port2]);
+      registration.active.postMessage({msgType: 'SAVE_PORT', [messageChannel.port2]);
     }, function(err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
   });
 }
+
+navigator.serviceWorker.controller.postMessage({msgType: 'UPDATE_CACHE', randomInfo: 'mad stuff'});
--- battlestar_demo_service_worker.js
+++ battlestar_demo_service_worker.js
@@ -32,24 +32,22 @@ self.addEventListener('fetch', function(event) {
   );
 });
 
+var messagePort;
+var localCacheForRaceCondition;
 self.addEventListener('message', event => {
   console.log("received msg from client", event);
-  event.waitUntil(async function() {
-    // Exit early if we don't have access to the client.
-    // Eg, if it's cross-origin.
-    if (!event.clientId) return;
-
-    // Get the client.
-    const client = await clients.get(event.clientId);
-    // Exit early if we don't get the client.
-    // Eg, if it closed.
-    if (!client) return;
-
-    // Send a message to the client.
-    client.postMessage({
-      msg: "Hey I just got a fetch from you!",
-      url: event.request.url
-    });
-   
-  }());
+  if (event.data.msgType == 'SAVE_PORT') {
+    messagePort = event.ports[0];
+    if (localCacheForRaceCondition) {
+      messagePort.postMessage(localCacheForRaceCondition);
+      localCacheForRaceCondition = undefined;
+    }
+  }
+  if (event.data.msgType == 'UPDATE_CACHE') {
+    if (messagePort) {
+      messagePort.postMessage(event.data);
+    } else {
+      localCacheForRaceCondition = event.data;
+    }
+  }
 });
